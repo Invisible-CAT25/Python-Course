195aa08a08fead2e2b712234066be20c8531d88d
Invisible-CAT25 <gargjatin2526@gmail.com>
2024-07-05 06:33:49 +0530

    new lectures uploaded

---
+++ lecture6.py
@@ -0,0 +1,14 @@
+# string = '''
+# string is a good'''
+
+# print(string)
+
+a = 'Python Programming'
+b = '             '
+
+# print(type(b))
+# print(len(b))
+# print(len(a))
+
+print(a[6])
+print(a)
---
+++ lecture7.py
@@ -0,0 +1,19 @@
+a = 'Python Programming'
+
+# print(a[:18:])
+# #rammi
+# print(a[::-2])
+# print(a[5:-1])
+
+b = "be active in class"
+# 'cti' , 'in c' , ' in ' , 'be active in class', 'b ci' , 'b' , 'lass' , 'active'
+
+c = 'hello'
+# 'oll' , 'olleh' , 'o'
+
+d = 'good day wasted'
+# 'day' , 'yad' , 'wse' , 'esw'
+
+print(b[9:13])
+print(c[::-1])
+print(d[9:14:2])
