413a1cc893035a3af8cd5ae0f2e5b6661eaea10c
Invisible-CAT25 <gargjatin2526@gmail.com>
2024-07-10 06:30:41 +0530

    lecture uploaded

---
+++ lecture8.py
@@ -0,0 +1,31 @@
+a = "Python Programming"
+# b = a.lower()
+# print(a.upper())
+# print(dir(str))
+# print(a.lower())
+# print(b)
+
+# print(a.swapcase())
+# print(a.count('y',0,6))
+# print(a.index('P'))
+# print(a.rindex('x'))
+# print(a.find('x'))
+
+b = "PYTHON Programming"
+# print(a.capitalize())
+# print(b.title())
+
+# print(dir(str))
+
+# print(a.startswith('m'))
+# print(b.isalpha())
+# print(b.isdigit())
+# print(b.isalnum())
+# print(b.isupper())
+# print(b.isspace())
+
+# print("-".join(b))
+# print(b.split('P'))
+# print(b.replace('P','L',1))
+print(b)
+print(b.strip('P'))
---
+++ lecture9.py
@@ -0,0 +1,6 @@
+a = "Python\r Programming"
+# \n -- new line , \b -- backspace , \t -- one tabspace , \r -- removed complete word
+print(a)
+
+b = r"Python\r Programm\ning"
+print(b)
