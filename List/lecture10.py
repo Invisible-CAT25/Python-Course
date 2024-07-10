0d36967fad405f8939402cc108c77df8ec9f5318
Invisible-CAT25 <gargjatin2526@gmail.com>
2024-07-10 11:32:47 +0530

    formatting of files done

---
+++ List/lecture10.py
@@ -0,0 +1,8 @@
+a = [2,3,45,67,89,"string",67.5,True,2,True]
+# print(len(a))
+# b = []
+# c = list([2,3,4,5,6])
+print(a)
+# print(b)
+# print(c)
+print(a[9])
---
+++ List/lecture11.py
@@ -0,0 +1,21 @@
+# a = [56,89,90,456,78.5,True,"hello",90,"world","instagram",[2,35,7,"facebook"],3+5j]
+# print(len(a))
+# print(a[1])
+# print(a[::3])
+# print(a[6][1:4])
+# print(a[10][3][0:5:2])
+
+# b = [[[[[[[[[[[[[[["python"]]]]]]]]]]]]]]]
+# c = ["python"]
+# print(b[0][0][0][0][0][0][0][0][0][0][0][0][0][0][0][1:4:2])
+
+
+# -- ques --
+a = ["hello","python","walmart"]
+# python , yth , mar
+b = ["goodmorning",["good night"],"bye"]
+# ['good night'] , ood , night , gdi
+c = [[["python",["sql"],["manual"],["java"]]]]
+# sql , ava , ual , pt
+d = [[[[[[[[[[[[[[[[[[[["python","programming"]]]]]]]]]]]]]]]]]]]]
+# pto , mmin
