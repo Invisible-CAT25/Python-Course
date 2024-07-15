afbd9962b0253117501202091d49310b5583e6cf
Invisible-CAT25 <gargjatin2526@gmail.com>
2024-07-15 21:58:43 +0530

    lecture-15 uploaded

---
+++ Tuple/lecture15.py
@@ -0,0 +1,22 @@
+# a = (25,89,78,79.7,True,"python",[78,90,89],7+5j,25)
+# print(a)
+
+# a = ()
+# a = tuple((67,89,34))
+# b = [89,90]
+# print(b)
+# b[0] = 0
+# print(b)
+# print(a[0])
+# print(len(a))
+
+# print(a[-9])
+# print(a[0:9:3])
+
+# ques
+# python , 7+5j , 25 to True , pto
+
+# print(a[5][0:4])
+
+b = 1,2,3,45,6,89
+print(b)
