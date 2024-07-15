dec93f09a3472e3997b2df8dc952d1890228c17c
Invisible-CAT25 <gargjatin2526@gmail.com>
2024-07-15 22:10:37 +0530

    lecture-16 uploaded

---
+++ Tuple/lecture16.py
@@ -0,0 +1,7 @@
+# print(dir(tuple))
+
+a = (67,89,90,45,1,1,1,2,3,4,5,2,2,2,2,2,2,2,22,2,2,2,2,2,2,2,"python","facebook","instagram","python",2,2,2,2,2,2,2,22,1,1,1,1,1,1,"worrld","python")
+# print(len(a))
+
+# print(a.count("python"))
+print(a.index(67))
