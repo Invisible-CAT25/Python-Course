a854bfd8a6d5115dbe2a3813be859026984bd9ae
Invisible-CAT25 <gargjatin2526@gmail.com>
2024-07-17 19:14:58 +0530

    lecture-18 uploaded

---
+++ Dictionary/lecture18.py
@@ -0,0 +1,29 @@
+a = {"hello":"hi","good":"bye","bad":"night"}
+# print(a)
+# a["hye"] = "morning"
+# print(a)
+# a["bad"] = "boy"
+# print(a)
+
+# print(dir(dict))
+# print(a.keys())
+# print(a.values())
+# print(a.items())
+# print(a)
+a.update({"hye":"morning"})
+# print(a)
+# print(a.setdefault("dark","color"))
+# print(a.setdefault("hello","color"))
+
+# print(a)
+# b = "hello"
+# c = [25,"python",89,"hello"]
+# print(dict.fromkeys(c,10))
+
+# print(a)
+# a.pop("good")
+# a.popitem()
+# a.popitem()
+print(a)
+a.clear()
+print(a)
