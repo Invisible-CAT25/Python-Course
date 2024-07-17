77646cf8f96259733084fc5f93ced2003507dc8a
Invisible-CAT25 <gargjatin2526@gmail.com>
2024-07-17 18:39:29 +0530

    lecture-17 uploaded

---
+++ Dictionary/lecture17.py
@@ -0,0 +1,13 @@
+a = {(2,3,5):10,2:20}
+
+# b = dict()
+
+# print(type(a))
+# print(type(b))
+
+# print(a[(2,3,5)])
+# print(a[(2,)])
+
+# print(type((2,)))
+
+print(a.get(5,'bhai phle key toh dict mein pass kar'))
