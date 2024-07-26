9d484941a537403cc76cd4ddfb71c23858094edd
Invisible-CAT25 <gargjatin2526@gmail.com>
2024-07-26 09:02:53 +0530

    lecture-22 uploaded

---
+++ Operators/lecture22.py
@@ -0,0 +1,37 @@
+# print(5+9)
+# a = 5
+# b = 2
+
+# -- Arithematic Operator
+
+# print(a+b)
+# print(a-b)
+# print(a*b)
+# print(a/b)
+# print(a%b)
+# print(a//b)
+# print(b**a)
+# print(divmod(a,b))
+
+# -- Assignment Operator
+# c = 10
+# c += 5
+# c = c+5
+# c -= 5
+# c *= 5
+# c //= 5
+# c %= 5
+# c **= 5
+# d = 5
+# c += d
+# print(c)
+
+# -- Relational Operator
+# d = 10
+# e = 10
+# print(d==e)
+# print(d!=e)
+# print(d<e)
+# print(d>e)
+# print(d<=e)
+# print(d>=e)
