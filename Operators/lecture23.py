e82793969e44a61b5d34de9505ff75e1306ea0ed
Invisible-CAT25 <gargjatin2526@gmail.com>
2024-07-29 17:21:14 +0530

    lecture-23 uploaded

---
+++ Operators/lecture23.py
@@ -0,0 +1,32 @@
+# Logical operator
+
+# a = 8
+# print(a>5 and a<9)
+# print(a<5 or a==9)
+# print(not(a>5))
+
+# membership operator
+# b = [12,345,7657,4,3124,654,6512,451345,13244,34,543,6,547,6,324,645,67]
+# print(345 not in b)
+# print(345 in b)
+
+# identity operator
+# b = 10
+# c = 20
+# d = 10
+
+# print(id(b))
+# print(id(c))
+# print(id(d))
+
+# print(b is d)
+# print(b is not c)
+
+# Bitwise operator
+a = 5
+b = 7
+print(bin(a))
+print(bin(b))
+print(a&b)
+print(a|b)
+print(~a)
