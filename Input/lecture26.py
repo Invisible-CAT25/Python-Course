891affc3349ed072cef16aa742245942d8eb4d29
Invisible-CAT25 <gargjatin2526@gmail.com>
2024-07-30 19:10:20 +0530

    ecture-26 uplaoded

---
+++ Input/lecture26.py
@@ -0,0 +1,14 @@
+# a = 5
+# b = 15
+# c = 10
+
+a,b,c = int(input("Enter the value of a : ")),int(input("Enter the value of b : ")),int(input("Enter the value of c : ")),
+# print(a)
+# print("The value of a is",a)
+# print("The value of a is",a,"and the value of b is",b,"and the value of c is",c)
+# print(f"The value of a is {a} and the value of b is {b} and the value of c is {c}")
+
+# print(a,b,c,sep='-')
+print(a,end=' ')
+print(b,end=' ')
+print(c)
