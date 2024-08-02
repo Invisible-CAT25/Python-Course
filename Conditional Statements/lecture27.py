b47816393f7409da20df11be54bb36fd9d0b29e1
Invisible-CAT25 <gargjatin2526@gmail.com>
2024-08-02 08:16:26 +0530

    lecture-27 uploaded

---
+++ Conditional Statements/lecture27.py
@@ -0,0 +1,16 @@
+# a = 5
+# print(a)
+
+# IF
+# a = 15
+# if a<10:
+#     print(a)
+
+# print("hello")
+
+# IF_ELSE
+a = 15
+if a<10:
+    print("a is less than 10")
+else:
+    print("a is greater than 10")
