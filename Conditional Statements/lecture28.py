042908179798974115423296e6809d41b52f638f
Invisible-CAT25 <gargjatin2526@gmail.com>
2024-08-03 08:54:38 +0530

    lecture-28 uplaoded

---
+++ Conditional Statements/lecture28.py
@@ -0,0 +1,37 @@
+# --------- Ques-1 -----------------------------
+
+# number = int(input("Enter a number : "))
+# if number%2 != 0:
+#     print("Number is odd")
+
+# ---------------- Ques-2 ------------------------
+
+# age = int(input("Enter your age : "))
+# if age>=18:
+#     print("Your are eligible for vote")
+
+# ----------------- Ques-3 -------------------------
+
+# num = int(input("Enter the number : "))
+# if num%2 == 0:
+#     print("Number is even")
+# else:
+#     print("Number is odd")
+
+# ---------------- Ques-4 ------------------------
+
+# age = int(input("Enter your age : "))
+# if age>=18:
+#     print("Your are eligible for vote")
+# else:
+#     print("You are not eligibble for vote")
+
+# ------------------- Ques-5 ------------------------
+
+a = "python"
+user = input("Enter the element you want to find in the string : ")
+
+if user in a:
+    print(f"{user} is present in the string!")
+else:
+    print(f"{user} is not present in the string")
