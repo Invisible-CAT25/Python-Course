f17415e9e3c5678e8ddf04743fb4144527d3693b
Invisible-CAT25 <gargjatin2526@gmail.com>
2024-08-04 10:01:14 +0530

    lecture-29 uploaded

---
+++ Conditional Statements/lecture29.py
@@ -0,0 +1,40 @@
+# a = 5
+# print(a)
+
+# IF
+# a = 15
+# if a<10:
+#     print(a)
+
+# print("hello")
+
+
+# IF_ELSE
+# a = 10
+# if a<10:
+#     print("a is less than 10")
+# else:
+#     print("a is greater than 10")
+
+
+# ELIF CONDITIONAL STATEMENT
+# a = 10
+# if a<10:
+#     print("a is less than 10")
+# elif a==10:
+#     print("a is equal to 10")
+# elif a==10:
+#     print("kuch ni")
+# else:
+#     print("a is greater than 10")
+
+
+# NESTED IF_ELSE
+a = 9
+if a<10:
+    if a==9:
+        print("a is equal to 9")
+    else:
+        print("a is not equal to 9")
+else:
+    print("a is graeter than 10")
