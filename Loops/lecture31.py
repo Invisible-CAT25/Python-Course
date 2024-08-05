f7e1792933cc7fe15bdc858fe437107a5f9b7c7b
Invisible-CAT25 <gargjatin2526@gmail.com>
2024-08-05 23:13:03 +0530

    lecture-31 uploaded

---
+++ Loops/lecture31.py
@@ -0,0 +1,24 @@
+# i = 0 #initialization
+
+# while i<500000: #condition
+#     print("Hello World!")
+#     i = i + 1 #updation
+
+# i = 1
+
+# while i<=10:
+#     print(i)
+#     # i = i + 1
+#     i += 1
+#     # i++ -- not acceptable in python
+
+# i = 0
+# while i <= 20:
+#     print(i,end=" ")
+#     i += 1
+
+i = 65
+# print(i)
+while i <= 90:
+    print(chr(i),end=" ")
+    i += 1
