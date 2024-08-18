732305ff50a055f6a1bc019cda7c0b3bddc46b30
Invisible-CAT25 <gargjatin2526@gmail.com>
2024-08-18 18:19:20 +0530

    lecture-32 uplaoded

---
+++ Loops/lecture32.py
@@ -0,0 +1,36 @@
+# Ques-1
+# i = 20
+
+# while i>=1:
+#     # if i%2==0:
+#     #     print(i,end=" ")
+#     print(i,end=" ")
+#     i = i-2
+
+# Ques-2  
+# s = "pyhton is a programming language"
+# a = input("Enter a character : ")
+# count = 0
+
+# i=0
+# while i<len(s):
+#     if a == s[i]:
+#         count += 1
+#     i = i+1
+
+# print(count)
+
+# Ques-3
+even = []
+odd = []
+
+i = 1
+while i<=20:
+    if i%2==0:
+        even.append(i)
+    else:
+        odd.append(i)
+    i = i+1
+
+print(even)
+print(odd)
