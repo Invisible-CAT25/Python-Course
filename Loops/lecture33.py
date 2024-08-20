2f02715a3c084c19304ebe5c102dc0a82dda6038
Invisible-CAT25 <gargjatin2526@gmail.com>
2024-08-20 14:07:29 +0530

    lecture-33 uplaoded

--- Loops/lecture33.py
+++ Loops/lecture33.py
@@ -1,25 +1,34 @@
-# Armstrong Number
-# num = int(input("Enter a Number: "))
-# num2 = num
+# # Armstrong Number
+
+# # a = 1234
+# num = int(input("Enter a Number : "))
+# # print(num)
+# temp = num
+# # print(type(num))
 # # print(len(num))
 # length = len(str(num))
 # # print(length)
 # arm = 0
 # while num!=0:
-#     arm = arm + (num%10)**length
-#     num = num//10
+#     arm += (num%10)**length
+#     num //= 10
 
-# if arm==num2:
-#     print("Given number is a armstrong number")
+# # print(num)
+# if arm==temp:
+#     print("Number is a armstrong number")
 # else:
-#     print("Given number is NOT A armstrong number")
+#     print("Number is not a armstrong number")
 
-# strong number
+# # a = 1234
+# # print(a%10)
+# # print(1234//10)
 
+# Strong Number
 num = 5
 fact = 1
-while num!=0:
-    fact = fact*num
-    num -= 1
+
+while num>0:
+    fact = fact * num
+    num = num - 1
 
 print(fact)
