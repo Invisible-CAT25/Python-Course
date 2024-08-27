0a8f4d3720492e7dbb89bb5922710e8a355259e0
Invisible-CAT25 <gargjatin2526@gmail.com>
2024-08-27 21:07:21 +0530

    lecture-41 uploaded

---
+++ Lambda Function/lecture41.py
@@ -0,0 +1,28 @@
+# a = lambda c,d : c*d
+# print(a(3,4))
+
+# def mul(a,b):
+#     return a+b
+
+# b = lambda a : a**2 if a%2==0 else a**3
+# print(b(4))
+
+# b = lambda a : (a**2,a**3)
+# print(b(5))
+
+# a = "mom"
+# b = lambda a : True if a==a[::-1] else False
+
+# print(b(a))
+
+# a = -7
+# b = lambda a : abs(a)
+
+# print(b(a))
+
+dic = {"hello":"hi","how":"are","sony":"live"}
+
+b = lambda dic : dic.keys()
+
+print(b(dic))
+
