d135abc113f9636f8d51a3231de024acb88de84f
Invisible-CAT25 <gargjatin2526@gmail.com>
2024-08-30 17:28:54 +0530

    lecture-42 uploaded

---
+++ Lambda Function/lecture42.py
@@ -0,0 +1,42 @@
+# Map Function
+
+# def func(a):
+#     return a**2
+
+# l = [1,2,3,4,5,6,7,8,9]
+
+# mapFunc = map(func,l)
+# print(list(mapFunc))
+
+# for i in mapFunc:
+#     print(i)
+
+# l = ["python","is","a","programming","language"]
+
+# a = map(lambda x : x[0],l)
+# print(list(a))
+
+# l = ["python","is","a","programming","language"]
+
+# a = map(len,l)
+# print(list(a))
+
+# names1 = ["dhruv","rohit","akash","monika","shruti"]
+
+# python = map(lambda x : {x:len(x)},names1)
+# print(list(python))
+
+
+# Ques-1
+# l1 = [2,3,4,5,6]
+# l2 = [3,4,5,8,9]
+
+# Ques-2
+# names = ["a","b","c","d","e"]
+# age = [20,35,21,89]
+
+# a = map(lambda x,y : {x:y},names,age)
+# print(list(a))
+
+# Ques-3 
+d = {10:"ten","hai":"value",(1,2,3):"python",1.2:"decimal"}
