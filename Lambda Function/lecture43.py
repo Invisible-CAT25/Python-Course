38181f784eb5055e4dd51e34f428648c9a35636a
Invisible-CAT25 <gargjatin2526@gmail.com>
2024-08-30 21:15:16 +0530

    lecture-43 uploaded

---
+++ Lambda Function/lecture43.py
@@ -0,0 +1,28 @@
+# l = [1,2,3,4,5,6,7,8,9,10,11,45,6,78,99]
+
+# a = filter(lambda x : x%2==0, l)
+# print(list(a))
+
+# l = ["lily flower","rose flower","banana tree","sun flower","lotus flower","neem tree"]
+
+# a = filter(lambda x : "flower" in x, l)
+# print(list(a))
+# for i in a:
+#     print(i)
+
+# l = ["apple","banana","gsamsun","grapes","onion","potato"]
+
+# a = filter(lambda x : len(x)%2==0, l)
+# print(list(a))
+
+# # Ques-1
+# l = [-10,29,-67,90,65,-78]
+
+# # Ques-2
+# a = ["hello","guys","good","morning"]
+
+# Reduce
+# a = filter()
+
+from functools import reduce
+a = reduce()
