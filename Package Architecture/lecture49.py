25431d2c5a50e069a0c8c17092f93734cc42b9ab
Invisible-CAT25 <gargjatin2526@gmail.com>
2024-09-12 21:39:31 +0530

    lecture-49 uploaded

---
+++ Package Architecture/lecture49.py
@@ -0,0 +1,14 @@
+# from copy import deepcopy
+# import copy
+# from copy import *
+
+# import x
+# from x import sub
+from x import *
+
+# x.sub(2,4)
+# x.mul(2,4)
+# x.sum(2,4)
+
+sub(2,3)
+mul(2,3)
