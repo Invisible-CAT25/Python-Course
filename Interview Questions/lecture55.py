3d617a1001ff6841c5c614c2d02df8de4aa6e42c
Invisible-CAT25 <gargjatin2526@gmail.com>
2024-09-17 21:43:28 +0530

    lecture-55 uploaded

---
+++ Interview Questions/lecture55.py
@@ -0,0 +1,32 @@
+# a = [10,20,30,[50]]
+# b = list(a)
+
+# a[3][0] = 2
+# a[1] = 1
+
+# print(b)
+
+# a = "python"
+
+# if a.islower().upper():
+#     print("hy")
+# else:
+#     print("hy"*3)
+
+# x = 1
+# while True:
+#     if x%5 = = 0:
+#         break
+#     print(x)
+#     x + = 1
+
+# x = ["xy","yz"]
+
+# for i in x:
+#     i.upper()
+
+# print(x)
+
+# s = set('abcde@')
+# print(s)
+# print('a' in s)
