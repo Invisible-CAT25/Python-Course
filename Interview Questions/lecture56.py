8638cbcf7284bd72ec594efcbc3bc7170de7647b
Invisible-CAT25 <gargjatin2526@gmail.com>
2024-09-18 19:05:21 +0530

    lecture-56 uploaded

---
+++ Interview Questions/lecture56.py
@@ -0,0 +1,33 @@
+# i = 0
+# while i<3:
+#     print(i)
+#     i+=1
+# else:
+#     print(0)
+
+# def example(a):
+#     aa = a+'1'
+#     aa = a*1
+#     return a
+# example("hello")
+
+# a = "cdf"
+# j = "j"
+# while j in a:
+#     print(j,end=" ")
+
+# x = "pqrs"
+# for i in range(len(x)):
+#     x[i].upper()
+# print(x)
+
+# x = "abcdef12"
+# print(x.replace('cd','12'))
+# print(x)
+
+# x = 56.236
+# print('%.2f'%x)
+
+string = "python is a language"
+for i in string:
+    print(i,end=",")
